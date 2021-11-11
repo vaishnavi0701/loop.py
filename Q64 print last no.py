a999c31346502d7c673c0cb29defe3c004361f09
vaishnavi0701 <90175670+vaishnavi0701@users.noreply.github.com>
2021-11-11 19:07:22 +0530

    Add files via upload

---
+++ Q64 print last no.py
@@ -0,0 +1,17 @@
+
+# num=int(input("enter the number"))
+# i=0
+# while i<num:
+#     a=num%10
+#     i=i+1
+# print(a)
+
+
+
+a=int(input("enter noi"))
+b=int(input("enter no"))
+i=0
+while i<=(a):
+    c=a/(1/b)
+    i=i+1
+print(c)
---
+++ Q65 without * do*.py
@@ -0,0 +1,9 @@
+
+
+a=int(input("enter noi"))
+b=int(input("enter no"))
+i=0
+while i<=(a):
+    c=a/(1/b)
+    i=i+1
+print(c)
